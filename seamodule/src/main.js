e447564f50518ff3f3abf76d4f83ca4d5421f88f
zhangjian <13522225365@163.com>
2020-08-13 17:41:02 +0800

    加载子应用的静态资源

--- seamodule/src/main.js
+++ seamodule/src/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import routers from './router'
 import VueCompositionApi from '@vue/composition-api'
+import './public-path'
 
 Vue.config.productionTip = false;
 Vue.use(VueCompositionApi);
